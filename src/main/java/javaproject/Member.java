b259996d547900483fec1e8b47914e47c4b20dfa
rock_it_twice <rock-it-twice@yandex.ru>
2021-10-01 12:23:17 +0300

    Fitness app v-0-01

---
+++ src/main/java/javaproject/Member.java
@@ -0,0 +1,61 @@
+package javaproject;
+
+/**
+ *
+ * @author Rock_it_twice
+ */
+public class Member {
+    // Fields
+    private char memberType;
+    private int memberID;
+    private String name;
+    private double fees;
+    
+    // Constructors    
+    public Member()
+    {
+        char pMemberType;
+        int pMemberID;
+        String pMemberName;
+        double pFees;
+        System.out.println("Member name: " + name + "/nMemberID: " + memberID);
+        System.out.println("-----------------------");
+    }
+    
+    //get- set- Methods
+    public void setMemberType(char pMemberType)
+    {
+        memberType = pMemberType;
+    }
+    public char getMemberType()
+    {
+        return memberType;
+    }
+    
+    public void setMemberID(int pMemberID)
+    {
+        memberID = pMemberID;
+    }
+    public int getMemberID()
+    {
+        return memberID;
+    }
+    
+    public void setName(String pMemberName)
+    {
+        name = pMemberName;
+    }
+    public String getName()
+    {
+        return name;
+    }
+    
+    public void setFees(double pFees)
+    {
+        fees = pFees;
+    }
+    public double getFees()
+    {
+        return fees;
+    }
+}
